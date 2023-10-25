7271c81b54706adef7be0ed9639f12556cf820b5
Hybee <hybee234@outlook.com>
2023-10-25 21:08:36 +1100

    starting on readme

--- index.js
+++ index.js
@@ -230,27 +230,3 @@ ${answers.usage_command}
         err ? console.log(err) : console.log('Successfully created README OUTPUT.md - stored in output folder')
         );
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
